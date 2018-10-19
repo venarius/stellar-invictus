e8d998ceab4ef59858596a22a1bd59a8fedf661a
Tim Lange <tim.lange@titanhq.de>
2018-10-19 10:53:34 +0000

    FIX: Fixed factions controller

--- app/controllers/factions_controller.rb
+++ app/controllers/factions_controller.rb
@@ -1,24 +1,28 @@
 class FactionsController < ApplicationController
    before_action :authenticate_user!
-   before_action :redirect_if_faction
+   before_action :redirect_if_faction, except: [:choose_faction]
    
    def index
       @factions = Faction.all
    end
    
    def choose_faction
-      current_user.faction = Faction.find(params[:id])
-      if !current_user.faction.nil? and current_user.save(validate: false)
-         redirect_to root_path
+      if current_user.faction
+        current_user.faction = Faction.find(params[:id])
+        if !current_user.faction and current_user.save(validate: false)
+           redirect_to root_path
+        else
+           flash[:error] = I18n.t('errors.something_went_wrong')
+           redirect_to factions_path
+        end
       else
-         flash[:error] = I18n.t('errors.something_went_wrong')
-         redirect_to factions_path
+        redirect_to root_path
       end
    end
    
    protected
    
    def redirect_if_faction
-      redirect_to root_path if current_user.faction
+      redirect_to root_path unless current_user.faction
    end
 end
