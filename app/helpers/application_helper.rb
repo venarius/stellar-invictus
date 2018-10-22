84d0c2c505681e82ae66b794b16ae739d75d0d90
Tim Lange <tim.lange@titanhq.de>
2018-10-22 13:20:18 +0000

    FIX: Bugfixing and removed map from js and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def navbar_item(path, name)
+    tmp = []
+    tmp << "<li class='nav-item #{'active' if current_page?(path)}'>"
+    tmp << "  <a href='#{path}' class='nav-link'>"
+    tmp << "    #{I18n.t(name)}" 
+    tmp << "      <span class='sr-only'>(current)</span>" if current_page?(path)
+    tmp << "  </a>"
+    tmp << "</li>"
+    tmp.join("\n").html_safe
+  end
 end
