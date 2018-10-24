9cc30ee04257565f0ffd1e97895c58b026a73ab4
Tim Lange <tim.lange@titanhq.de>
2018-10-24 09:39:00 +0000

    NEW: Styled few more pages of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'connect', sign_out: 'disconnect', sign_up: 'enlist'}
+  devise_for :users, path: '', path_names: { sign_in: 'connect', sign_out: 'disconnect', sign_up: 'enlist', edit: 'pilot' }
   
   root 'static_pages#home'
   
