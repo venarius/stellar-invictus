917b9507386d6b44763efe579dd494e4432a158f
Tim Lange <tim.lange@titanhq.de>
2018-10-26 20:07:22 +0000

    FIX: Hotfix Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:venarius/stellarInvictusRails.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/tla/app/stellar"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
