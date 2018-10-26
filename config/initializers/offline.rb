831a5938f67d4a4a016c32f2c0acd8c31e7cb020
Tim Lange <tim.lange@titanhq.de>
2018-10-26 20:19:05 +0000

    FIX: Capistrano offline initializer

--- config/initializers/offline.rb
+++ config/initializers/offline.rb
@@ -1,3 +1,5 @@
-User.all.each do |user|
-   user.update_columns(online: false) 
+if ActiveRecord::Base.connection.table_exists? 'users'
+  User.all.each do |user|
+     user.update_columns(online: false) 
+  end
 end
