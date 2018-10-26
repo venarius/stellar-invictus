55a21ba58b4e0594f75c8b1c547aa2b1cdd0631e
Tim Lange <tim.lange@titanhq.de>
2018-10-26 07:36:39 +0000

    NEW: Better styling for overview and players

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,5 +3,6 @@ class GameController < ApplicationController
       @current_user = User.includes(:system).find(current_user.id)
       @local_messages = ChatMessage.includes(:user).where(system: current_user.system).last(10)
       @global_messages = ChatMessage.includes(:user).where(system: nil).last(10)
+      @local_users = User.where(system: current_user.system, online: true)
    end
 end
