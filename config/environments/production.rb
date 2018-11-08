939305694180bd5fa978d2398a1d392dcde90654
Tim Lange <tim.lange@titanhq.de>
2018-11-08 10:55:27 +0000

    NEW: Added cloudfront as cdn for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # CDN
+  config.action_controller.asset_host = 'd2kowkrdxzv3xp.cloudfront.net'
 end
