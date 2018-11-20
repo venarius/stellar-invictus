a335eec02956d029389b4c05ddb9ec7b325aba1f
Tim Lange <tim.lange@titanhq.de>
2018-11-20 14:41:48 +0000

    REF: Refactored with rubycritic

--- app/workers/disappear_worker.rb
+++ app/workers/disappear_worker.rb
@@ -4,40 +4,39 @@ class DisappearWorker
 
   def perform(player_id)
     user = User.find(player_id)
-    if user
-      # Remove one from online
-      user.update_columns(online: user.online - 1) if user.online > 0
-      
-      # If user is not docked and now closed last connection
-      if !user.docked and user.online == 0
+    system = user.system
+    # Remove one from online
+    online = user.online
+    user.update_columns(online: online - 1) if online > 0
+    
+    # If user is not docked and now closed last connection
+    if user.reload.online == 0
+      unless user.docked
         # Tell everyone in location that user warped out
         ActionCable.server.broadcast("location_#{user.location.id}", method: 'player_warp_out', name: user.full_name)
         
         # Remove user as target from every player that targeted him
-        User.where(target_id: user.id).each do |u|
-          u.update_columns(target_id: nil)
-          ActionCable.server.broadcast("player_#{u.id}", method: 'refresh_target_info')
+        User.where(target_id: user.id).each do |useri|
+          useri.update_columns(target_id: nil)
+          ActionCable.server.broadcast("player_#{useri.id}", method: 'refresh_target_info')
         end
       end
       
-      # Remove user target and mining target if logging off
-      if user.online == 0
-        
-        user.update_columns(target_id: nil, mining_target_id: nil, npc_target_id: nil, is_attacking: false)
-        
-        # Tell everyone in system to update their local players
-        user.system.locations.each do |location|
-          ActionCable.server.broadcast("location_#{location.id}", method: 'update_players_in_system', 
-            count: User.where("online > 0").where(system: user.system).count, 
-            names: User.where("online > 0").where(system: user.system).map(&:full_name))
-        end
-        
-        # Tell all users in custom chat channels to update
-        user.chat_rooms.where(chatroom_type: 'custom').each do |room|
-          ChatChannel.broadcast_to(room, method: 'update_players', names: room.users.where("online > 0").map(&:full_name))
-        end
-        
+      # Update user
+      user.update_columns(target_id: nil, mining_target_id: nil, npc_target_id: nil, is_attacking: false)
+      
+      # Tell everyone in system to update their local players
+      users = User.where("online > 0").where(system: system)
+      system.locations.each do |location|
+        ActionCable.server.broadcast("location_#{location.id}", method: 'update_players_in_system', 
+          count: users.count, names: users.map(&:full_name))
+      end
+      
+      # Tell all users in custom chat channels to update
+      user.chat_rooms.where(chatroom_type: 'custom').each do |room|
+        ChatChannel.broadcast_to(room, method: 'update_players', names: room.users.where("online > 0").map(&:full_name))
       end
     end
+      
   end
 end
--- app/workers/mining_worker.rb
+++ app/workers/mining_worker.rb
@@ -7,7 +7,7 @@ class MiningWorker
     asteroid = Asteroid.find(asteroid_id)
     
     # Cancel if Player already mining this
-    return if player.mining_target_id == asteroid.id
+    return if player.mining_target_id == asteroid_id
     
     # Untarget combat target if player is targeting mining target
     unless player.target_id == nil
@@ -19,23 +19,23 @@ class MiningWorker
     player.update_columns(npc_target_id: nil, is_attacking: false)
     
     # Mine every 30 seconds
-    player.update_columns(mining_target_id: asteroid.id)
-    ActionCable.server.broadcast("player_#{player.id}", method: 'refresh_target_info')
+    player.update_columns(mining_target_id: asteroid_id)
+    ActionCable.server.broadcast("player_#{player_id}", method: 'refresh_target_info')
     while true do
       30.times do
         return unless can_mine(player, asteroid)
         sleep(1)
       end
       asteroid.update_columns(resources: asteroid.resources - 100)
-      ActionCable.server.broadcast("player_#{player.id}", method: 'update_asteroid_resources', resources: asteroid.resources)
+      ActionCable.server.broadcast("player_#{player_id}", method: 'update_asteroid_resources', resources: asteroid.resources)
       item = Item.create(spaceship_id: player.active_spaceship.id, loader: "asteroid.#{asteroid.asteroid_type}")
-      ActionCable.server.broadcast("player_#{player.id}", method: 'refresh_player_info')
+      ActionCable.server.broadcast("player_#{player_id}", method: 'refresh_player_info')
       
       # Log
-      ActionCable.server.broadcast("player_#{player.id}", method: 'log', text: I18n.t('log.you_mined_from_asteroid', ore: item.get_attribute('name').downcase) )
+      ActionCable.server.broadcast("player_#{player_id}", method: 'log', text: I18n.t('log.you_mined_from_asteroid', ore: item.get_attribute('name').downcase) )
       
       # Tell other users who miner this rock to also update their resources
-      User.where(mining_target_id: asteroid.id).where("online > 0").each do |u|
+      User.where(mining_target_id: asteroid_id).where("online > 0").each do |u|
         ActionCable.server.broadcast("player_#{u.id}", method: 'update_asteroid_resources_only', resources: asteroid.resources)
       end
       
@@ -51,19 +51,19 @@ class MiningWorker
     
     # Remove asteroid as target if depleted
     if asteroid.resources < 100
-      ActionCable.server.broadcast("player_#{player.id}", method: 'asteroid_depleted')
+      ActionCable.server.broadcast("player_#{player_id}", method: 'asteroid_depleted')
       player.update_columns(mining_target_id: nil)
       return false
     end
     
     # Stop mining if user has other or no mining target
-    if player.mining_target_id != asteroid.id || player.target_id != nil
+    if player.mining_target_id != asteroid_id || player.target_id != nil
       return false
     end
     
     # Stop mining if player's ship is full
     if player.active_spaceship.get_weight >= player.active_spaceship.get_attribute('storage')
-      ActionCable.server.broadcast("player_#{player.id}", method: 'asteroid_depleted')
+      ActionCable.server.broadcast("player_#{player_id}", method: 'asteroid_depleted')
       player.update_columns(mining_target_id: nil)
       return false
     end
