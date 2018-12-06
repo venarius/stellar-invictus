898f2272c113855b7ce1ca276451db058f60b800
Tim Lange <tim.lange@titanhq.de>
2018-12-06 14:42:26 +0000

    NEW: Added market tests

---
+++ spec/controllers/market_controller_spec.rb
@@ -0,0 +1,200 @@
+require 'rails_helper'
+
+RSpec.describe MarketController, type: :controller do
+  context 'without login' do
+    describe 'GET list' do
+      it 'should redirect_to new_user_session_path' do
+        get :list
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    describe 'GET search' do
+      it 'should redirect_to new_user_session_path' do
+        get :search
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    describe 'POST buy' do
+      it 'should redirect_to new_user_session_path' do
+        post :buy
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    describe 'POST sell' do
+      it 'should redirect_to new_user_session_path' do
+        post :sell
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    describe 'POST appraisal' do
+      it 'should redirect_to new_user_session_path' do
+        post :appraisal
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+  
+  context 'with login' do
+    before(:each) do
+      @user = FactoryBot.create(:user_with_faction)
+      sign_in @user
+      @user.update_columns(location_id: Location.where(location_type: 'station').first.id, docked: true)
+    end
+    
+    describe 'GET list' do
+      it 'should render template' do
+        get :list, params: {loader: 'test'}
+        expect(response.status).to eq(200)
+        expect(response).to render_template('stations/market/_list')
+      end
+      
+      it 'should not render template if no params' do
+        get :list
+        expect(response.status).to eq(400)
+      end
+    end
+    
+    describe 'GET search' do
+      it 'should render template' do
+        get :search, params: {search: 'test'}
+        expect(response.status).to eq(200)
+        expect(response).to render_template('stations/market/_list')
+      end
+      
+      it 'should not render template if no params' do
+        get :search
+        expect(response.status).to eq(400)
+      end
+    end
+    
+    describe 'POST appraisal' do
+      it 'should response with price' do
+        post :appraisal, params: {loader: 'asteroid.nickel', quantity: "10", type: "item"}
+        expect(response.status).to eq(200)
+        expect(response.body).to include("15")
+      end
+      
+      it 'should not response with price if shit given' do
+        post :appraisal, params: {loader: 'noot.noot', quantity: "10", type: "noot"}
+        expect(response.status).to eq(400)
+      end
+    end
+    
+    describe 'POST buy' do
+      before(:each) do
+        @user.update_columns(units: 5000)
+        @item = MarketListing.create(location: @user.location, loader: 'asteroid.nickel', listing_type: 'item', price: 1000, amount: 1)  
+        @ship = MarketListing.create(location: @user.location, loader: 'Chronos', listing_type: 'ship', price: 2000, amount: 1)  
+      end
+      
+      it 'should buy item if has enough money' do
+        post :buy, params: {id: @item.id, amount: "1"}
+        expect(response.status).to eq(200)
+        expect(@user.reload.units).to eq(4000)
+        expect(Item.count).to eq(1)
+      end
+      
+      it 'should buy ship if has enough money' do
+        post :buy, params: {id: @ship.id, amount: "1"}
+        expect(response.status).to eq(200)
+        expect(@user.reload.units).to eq(3000)
+        expect(Spaceship.count).to eq(2)
+        expect(Item.count).to eq(0)
+      end
+      
+      it 'should not buy item if not enough money' do
+        @user.update_columns(units: 50)
+        post :buy, params: {id: @item.id, amount: "1"}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(50)
+      end
+      
+      it 'should not buy item if in other location' do
+        @user.update_columns(location_id: Location.last.id)
+        post :buy, params: {id: @item.id, amount: "1"}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(5000)
+      end
+      
+      it 'should not buy more items than available' do
+        post :buy, params: {id: @item.id, amount: "3"}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(5000)
+      end
+    end
+    
+    describe 'POST sell' do
+      before(:each) do
+        @item = Item.create(location: @user.location, user: @user, loader: 'asteroid.nickel')
+        @ship = Spaceship.create(name: 'Nano', user: @user, hp: 50, location: @user.location)
+      end
+      
+      it 'should sell item' do
+        post :sell, params: {loader: 'asteroid.nickel', type: 'item', quantity: '1'}
+        expect(response.status).to eq(200)
+        expect(@user.reload.units).to eq(1002)
+        expect(Item.count).to eq(0)
+      end
+      
+      it 'should sell item on price of listing' do
+        MarketListing.create(location: @user.location, loader: 'asteroid.nickel', listing_type: 'item', price: 1000, amount: 1)  
+        post :sell, params: {loader: 'asteroid.nickel', type: 'item', quantity: '1'}
+        expect(response.status).to eq(200)
+        expect(@user.reload.units).to eq(1002)
+        expect(Item.count).to eq(0)
+      end
+      
+      it 'should sell not more items than user has' do
+        post :sell, params: {loader: 'asteroid.nickel', type: 'item', quantity: '2'}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(1000)
+        expect(Item.count).to eq(1)
+      end
+      
+      it 'should sell ship' do
+        post :sell, params: {loader: @ship.name, id: @ship.id, type: 'ship', quantity: '1'}
+        expect(response.status).to eq(200)
+        expect(@user.reload.units).to eq(1000)
+        expect(Spaceship.count).to eq(1)
+      end
+      
+      it 'should not sell ship if selling more' do
+        post :sell, params: {loader: @ship.name, id: @ship.id, type: 'ship', quantity: '2'}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(1000)
+        expect(Spaceship.count).to eq(2)
+      end
+      
+      it 'should not sell ship which is not here' do
+        @ship.update_columns(location_id: Location.last.id)
+        post :sell, params: {loader: @ship.name, id: @ship.id, type: 'ship', quantity: '1'}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(1000)
+        expect(Spaceship.count).to eq(2)
+      end
+      
+      it 'should not sell active spaceship' do
+        post :sell, params: {loader: @user.active_spaceship.name, id: @user.active_spaceship.id, type: 'ship', quantity: '1'}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(1000)
+        expect(Spaceship.count).to eq(2)
+      end
+      
+      it 'should not sell shit' do
+        post :sell, params: {loader: 'Blub', type: 'ship', quantity: '1'}
+        expect(response.status).to eq(400)
+        expect(@user.reload.units).to eq(1000)
+        expect(Spaceship.count).to eq(2)
+      end
+    end
+  end
+end
--- spec/models/market_listing_spec.rb
+++ spec/models/market_listing_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe MarketListing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe MarketListing do
+  context 'new market_listing' do
+    describe 'attributes' do
+      it { should respond_to :location }
+      it { should respond_to :price }
+      it { should respond_to :amount }
+      it { should respond_to :loader }
+    end
+    
+    describe 'Relations' do
+      it { should belong_to :location }
+    end
+    
+    describe 'Enums' do
+       it { should define_enum_for(:listing_type).with([:item, :ship]) } 
+    end
+    
+  end
 end
