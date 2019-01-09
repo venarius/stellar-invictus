2c9e6e8b520b8aeb6087709ba2fbe27d9097fdba
Tim Lange <tim.lange@titanhq.de>
2019-01-09 21:12:50 +0000

    FIX: Small fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "94.237.57.202", user: "deploy", roles: %w{web app db}
+server "94.237.57.202", user: "deploy", roles: %w{web app}
 server "94.237.61.32", user: "deploy", roles: %w{db}
 
 
