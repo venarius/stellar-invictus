0a37ee35ef9bbcdd0871b36f0e1f2991c2803b28
Tim Lange <tim.lange@titanhq.de>
2019-01-09 21:33:56 +0000

    NEW: Updated to ruby 2.5.3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server "94.237.57.202", user: "deploy", roles: %w{web app}
-server "94.237.61.32", user: "deploy", roles: %w{db}
+server "94.237.57.202", user: "deploy", roles: %w{app web}
+server "db.stellar-invictus.com", user: "deploy", roles: %w{db}
 
 
 # role-based syntax
