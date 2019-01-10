e3b60067c94335c958b31e29546e674e4802e8ff
Tim Lange <tim.lange@titanhq.de>
2019-01-10 08:02:19 +0000

    NEW: Changed deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,20 +56,3 @@ namespace :deploy do
     end
   end
 end
-
-namespace :sidekiq do
-  task :quiet do
-    on roles(:app) do
-      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP") 
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :initctl, :restart, :workers
-    end
-  end
-end
-
-after 'deploy:starting', 'sidekiq:quiet'
-after 'deploy:reverted', 'sidekiq:restart'
-after 'deploy:published', 'sidekiq:restart'
