399c6372626e2dce5084074eb196c7cf2487a232
Tim Lange <tim.lange@titanhq.de>
2019-01-16 13:08:52 +0000

    FIX: Fixed some stuff

--- runner/hidden_enemies.rb
+++ runner/hidden_enemies.rb
@@ -1,8 +1,8 @@
 User.where.not(online: 0).order(Arel.sql("RANDOM()")).limit(20).each do |user|
   if (user.location.mission? || user.location.exploration_site?) and user.can_be_attacked
-    if rand(1..11) == 11
-      rand(1..3).times do
-        EnemyWorker.perform_async(nil, user.location_id, nil, nil, nil, true)
+    if rand(1..6) == 6
+      rand(2..4).times do
+        EnemyWorker.perform_async(nil, user.location_id)
       end
     end
   end
