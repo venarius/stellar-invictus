52964562d884632060923dfe12316a1f2f86401c
Tim Lange <tim.lange@titanhq.de>
2019-01-17 17:03:15 +0000

    FIX: Fixed some annoying bugs

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -1,7 +1,7 @@
 class NpcsController < ApplicationController
   def target
     if params[:id] and current_user.can_be_attacked
-      target = Npc.find(params[:id]) rescue nil
+      target = Npc.find(params[:id].to_i) rescue nil
       if target and target.location == current_user.location and current_user.npc_target != target
         TargetNpcWorker.perform_async(current_user.id, target.id)
         render json: {time: current_user.active_spaceship.get_target_time}, status: 200 and return
