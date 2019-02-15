d15114b7156d6be20a4c8729406f5734ad226258
Tim Lange <tim.lange@titanhq.de>
2019-02-15 17:08:34 +0000

    FIX: Some fixes for faction choose

--- runner/asteroids_redo.rb
+++ runner/asteroids_redo.rb
@@ -2,19 +2,16 @@
 Location.where(location_type: 'asteroid_field').each do |loc|
   if loc.asteroids.count < 5
     if loc.system.low?
-      rand(2..5).times do 
-        Asteroid.create(location: loc, asteroid_type: [0,1,2].sample, resources: 35000)
-      end
-      rand(1..3).times do
-        Asteroid.create(location: loc, asteroid_type: 5, resources: 35000)
+      rand(2..7).times do 
+        Asteroid.create(location: loc, asteroid_type: rand(6), resources: 350000)
       end
     elsif loc.system.wormhole?
       rand(1..3).times do 
-        Asteroid.create(location: loc, asteroid_type: 6, resources: 35000)
+        Asteroid.create(location: loc, asteroid_type: 6, resources: 350000)
       end
     else
       rand(2..5).times do 
-        Asteroid.create(location: loc, asteroid_type: rand(4), resources: 35000)
+        Asteroid.create(location: loc, asteroid_type: rand(4), resources: 350000)
       end
     end
   end
