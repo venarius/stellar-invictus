acfe454dd7148f4ec40d376c63787397a1191791
Tim Lange <tim.lange@titanhq.de>
2019-02-22 16:27:06 +0000

    FIX: Some hotfixes

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -11,16 +11,18 @@ $( document ).on('turbolinks:load', function() {
   $('.mobile-menu-open-btn').on('click', function() {
     $('body').css('padding-bottom', '0');
     $('.navbar.main-navbar').fadeIn("fast", function() {
-      $('#app-container').fadeOut("fast");
-      $('.mobile-nav').fadeOut("fast");
+      $('.mobile-nav').fadeOut("fast", function() {
+        $('#app-container').fadeOut("fast");
+      });
     });
   });
   
   $('.mobile-menu-close-btn').on('click', function() {
     $('body').css('padding-bottom', '50px');
-    $('#app-container').fadeIn("fast", function() {
-      $('.mobile-nav').fadeIn("fast");
-      $('.navbar.main-navbar').fadeOut("fast");
+    $('#app-container').fadeIn(1, function() {
+      $('.mobile-nav').fadeIn(1, function() {
+        $('.navbar.main-navbar').fadeOut("fast");
+      });
     });
   });
   
