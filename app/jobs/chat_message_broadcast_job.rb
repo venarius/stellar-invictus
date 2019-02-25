3c6e72699fe5c83e8351d31ad7d5bb37cc44c8c2
Tim Lange <tim.lange@titanhq.de>
2019-02-25 19:49:41 +0000

    NEW: Added killboard

--- app/jobs/chat_message_broadcast_job.rb
+++ app/jobs/chat_message_broadcast_job.rb
@@ -5,8 +5,9 @@ class ChatMessageBroadcastJob < ApplicationJob
     ChatChannel.broadcast_to(chat_message.chat_room, message: render_message(chat_message))
   end 
 
-  private 
-    def render_message(chat_message) 
-      ApplicationController.renderer.render(partial: 'chat_messages/message', locals: { message: chat_message }) 
-    end 
+  private
+  
+  def render_message(chat_message) 
+    ApplicationController.renderer.render(partial: 'chat_messages/message', locals: { message: chat_message }) 
+  end 
 end  
