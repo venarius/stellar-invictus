eb590390de4eeab8b0182966297a63cf5b91b873
Tim Lange <tim.lange@titanhq.de>
2019-03-19 19:09:42 +0000

    FIX: Fixed js markdown xss

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,2 +1,2 @@
-renderer = Redcarpet::Render::HTML.new(no_images: true, filter_html: true, no_styles: true)
+renderer = Redcarpet::Render::HTML.new(no_images: true, filter_html: true, no_styles: true, safe_links_only: true)
 MARKDOWN = Redcarpet::Markdown.new(renderer, autolink: true, tables: true)
