bd678092927831fc1182a283d25d13bc5d99d6e2
Ben Sharpe <bsharpe@gmail.com>
2019-04-03 14:18:49 -0600

    remove delegates

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -48,6 +48,4 @@ class Mission < ApplicationRecord
   enum mission_type:   [:tutorial, :delivery, :combat, :mining, :market, :vip]
   enum mission_status: [:offered, :active, :failed, :completed]
   enum difficulty:     [:easy, :medium, :hard]
-
-  delegate :reputation_1, :reputation_2, :reputation_3, to: :user, prefix: true
 end
