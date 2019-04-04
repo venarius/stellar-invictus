12cd7a3e657f5b2ee445adc92bfa24221272a1a2
Ben Sharpe <bsharpe@gmail.com>
2019-04-04 09:24:25 -0600

    comment how to show errors

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -3,18 +3,14 @@
 class ApplicationService < LightServiceObject::Base
   def self.failed(error)
     return nil if Rails.env.production?
+    # Comment out this line during testing to see the errors w/ a backtrace
     return nil if Rails.env.test?
 
-    puts "#{self.name} FAILED".red
     root = Rails.root.to_s
+    ap "#{self.name} FAILED"
     ap error
       .backtrace
       .select { |e| e.index(root) }
       .map { |e| e.gsub(root, "") }
-  end
-
-  def error_reason(error)
-    # Give subclasses a chance to see errors first
-    "#{error}"
   end
 end
