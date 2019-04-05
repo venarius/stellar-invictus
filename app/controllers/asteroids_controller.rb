99ae08f7317e4b5d19a2f0339acb738c40fd2ade
Ben Sharpe <bsharpe@gmail.com>
2019-04-04 19:57:59 -0600

    add status: :ok

--- app/controllers/asteroids_controller.rb
+++ app/controllers/asteroids_controller.rb
@@ -5,7 +5,7 @@ class AsteroidsController < ApplicationController
     if svc_result.failure?
       render json: { error_message: svc_result.failure }, status: :bad_request
     else
-      render json: svc_result.value!
+      render json: svc_result.value!, status: :ok
     end
   end
 
