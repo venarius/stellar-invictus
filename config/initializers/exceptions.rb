27f11a909d6556089eadade3dc2013d449754fa6
Ben Sharpe <bsharpe@gmail.com>
2019-04-05 15:13:17 -0600

    add a RedirectRequest exception, used in FactionController

--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,1 +1,10 @@
 class InvalidRequest < StandardError; end
+class RedirectRequest < StandardError
+  attr_reader :error
+
+  def initialize(route, error: nil)
+    @error = error
+    super(route)
+  end
+  alias route message
+end
