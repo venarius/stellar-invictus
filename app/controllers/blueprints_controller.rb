81552d55fc84fedea627e0e7defb4c8741e2c4f5
Ben Sharpe <bsharpe@gmail.com>
2019-04-05 13:44:07 -0600

    fix case

--- app/controllers/blueprints_controller.rb
+++ app/controllers/blueprints_controller.rb
@@ -7,7 +7,7 @@ class BlueprintsController < ApplicationController
     raise InvalidRequest unless %[item ship].include?(params[:type])
     raise InvalidRequest if current_user.has_blueprints_for?(params[:loader])
 
-    price = case params[:type] do
+    price = case params[:type]
       when 'item'
         raise ArgumentError.new("Unknown Item") unless Item.get_attributes(params[:loader])
         Item.get_attribute(params[:loader], :price) * 20
