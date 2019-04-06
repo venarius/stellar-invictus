003e605aa0020c6e0a346bb218816aa8695e25d2
Ben Sharpe <bsharpe@gmail.com>
2019-04-06 08:22:19 -0600

    convert polls_controller to exceptions

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,44 +1,47 @@
 class PollsController < ApplicationController
-  before_action :get_poll, only: [:upvote, :downvote, :move_up, :delete]
-  before_action :check_voting_right, only: [:upvote, :downvote]
 
   def create
-    if params[:question] && params[:link] && current_user.admin?
-      Poll.create(status: :active, question: params[:question], forum_link: params[:link])
-      render(json: {}, status: :ok) && (return)
-    end
-    render json: {}, status: :bad_request
+    raise InvalidRequest unless params[:question]
+    raise InvalidRequest unless params[:link]
+    raise InvalidRequest unless current_user.admin?
+
+    Poll.create(status: :active, question: params[:question], forum_link: params[:link])
+    render json: {}, status: :ok
   end
 
   def upvote
-    @poll.upvote_from current_user if @poll.active?
-    render json: { upvotes: ((@poll.get_upvotes.size.to_f / (@poll.votes_for.size / 100.0) rescue 0) rescue 0), downvotes: ((@poll.get_downvotes.size.to_f / (@poll.votes_for.size / 100.0) rescue 0) rescue 0), votes: @poll.votes_for.size }, status: :ok
+    poll
+    raise InvalidRequest.new('errors.need_1000_credits_to_be_eligible_to_vote') if current_user.units < 1000
+    poll.upvote_from current_user if poll.active?
+    render json: { upvotes: poll.upvote_pct, downvotes: poll.downvote_pct, votes: poll.votes_for.size }, status: :ok
   end
 
   def downvote
-    @poll.downvote_from current_user if @poll.active?
-    render json: { upvotes: ((@poll.get_upvotes.size.to_f / (@poll.votes_for.size / 100.0) rescue 0) rescue 0), downvotes: ((@poll.get_downvotes.size.to_f / (@poll.votes_for.size / 100.0) rescue 0) rescue 0), votes: @poll.votes_for.size }, status: :ok
+    poll
+    raise InvalidRequest.new('errors.need_1000_credits_to_be_eligible_to_vote') if current_user.units < 1000
+    poll.downvote_from current_user if poll.active?
+    render json: { upvotes: poll.upvote_pct, downvotes: poll.downvote_pct, votes: poll.votes_for.size }, status: :ok
   end
 
   def move_up
-    @poll.move_up if current_user.admin
+    poll
+    poll.move_up if current_user.admin
     render json: {}, status: :ok
   end
 
   def delete
-    @poll.destroy if current_user.admin
+    poll
+    poll.destroy if current_user.admin
     render json: {}, status: :ok
   end
 
   private
 
-  def get_poll
-    @poll = Poll.ensure(params[:id])
-    render json: {}, status: :bad_request if @poll.nil?
+  def poll
+    @poll ||= begin
+      record = Poll.ensure(params[:id])
+      raise InvalidRequest unless record
+      record
+    end
   end
-
-  def check_voting_right
-    render json: { error_message: I18n.t('errors.need_1000_credits_to_be_eligible_to_vote') }, status: :bad_request if current_user.units < 1000
-  end
-
 end
--- spec/controllers/polls_controller_spec.rb
+++ spec/controllers/polls_controller_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe PollsController, type: :controller do
   context 'with login' do
+    let(:user) { create :user_with_faction, units: 1000 }
+    let(:poll) { create :poll, status: :active }
     before (:each) do
-      @user = create(:user_with_faction, units: 1000)
-      sign_in @user
+      sign_in user
     end
 
     describe 'POST create' do
       it 'should create poll as admin' do
-        @user.update(admin: true)
+        user.update(admin: true)
         post :create, params: { question: "Test", link: "Test" }
         expect(response).to have_http_status(:ok)
         expect(Poll.count).to eq(1)
@@ -22,7 +23,7 @@ RSpec.describe PollsController, type: :controller do
       end
 
       it 'should not create poll as admin but with no params' do
-        @user.update(admin: true)
+        user.update(admin: true)
         post :create
         expect(response).to have_http_status(:bad_request)
         expect(Poll.count).to eq(0)
@@ -30,99 +31,83 @@ RSpec.describe PollsController, type: :controller do
     end
 
     describe 'POST upvote' do
-      before (:each) do
-        @poll = create(:poll, status: :active)
-      end
-
       it 'should upvote poll' do
-        post :upvote, params: { id: @poll.id }
+        post :upvote, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.get_upvotes.size).to eq(1)
+        expect(poll.get_upvotes.size).to eq(1)
       end
 
       it 'should not upvote poll if not active' do
-        @poll.waiting!
-        post :upvote, params: { id: @poll.id }
+        poll.waiting!
+        post :upvote, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.get_upvotes.size).to eq(0)
+        expect(poll.get_upvotes.size).to eq(0)
       end
 
       it 'should not upvote poll if not enough credits' do
-        @user.update(units: 100)
-        post :upvote, params: { id: @poll.id }
+        user.update(units: 100)
+        post :upvote, params: { id: poll.id }
         expect(response).to have_http_status(:bad_request)
-        expect(@poll.get_upvotes.size).to eq(0)
+        expect(poll.get_upvotes.size).to eq(0)
       end
     end
 
     describe 'POST downvote' do
-      before (:each) do
-        @poll = create(:poll, status: :active)
-      end
-
       it 'should upvote poll' do
-        post :downvote, params: { id: @poll.id }
+        post :downvote, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.get_downvotes.size).to eq(1)
+        expect(poll.get_downvotes.size).to eq(1)
       end
 
       it 'should not upvote poll if not active' do
-        @poll.waiting!
-        post :downvote, params: { id: @poll.id }
+        poll.waiting!
+        post :downvote, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.get_downvotes.size).to eq(0)
+        expect(poll.get_downvotes.size).to eq(0)
       end
 
       it 'should not downvote poll if not enough credits' do
-        @user.update(units: 100)
-        post :downvote, params: { id: @poll.id }
+        user.update(units: 100)
+        post :downvote, params: { id: poll.id }
         expect(response).to have_http_status(:bad_request)
-        expect(@poll.get_downvotes.size).to eq(0)
+        expect(poll.get_downvotes.size).to eq(0)
       end
     end
 
     describe 'POST move_up' do
-      before (:each) do
-        @poll = create(:poll, status: :active)
-      end
-
       it 'should move poll up as admin' do
-        @poll.waiting!
-        @user.update(admin: true)
-        post :move_up, params: { id: @poll.id }
+        poll.waiting!
+        user.update(admin: true)
+        post :move_up, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.reload.in_progress?).to be_truthy
+        expect(poll.reload.in_progress?).to be_truthy
       end
 
       it 'should not move poll up as non admin' do
-        @poll.waiting!
-        post :move_up, params: { id: @poll.id }
+        poll.waiting!
+        post :move_up, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
-        expect(@poll.reload.in_progress?).to be_falsey
+        expect(poll.reload.in_progress?).to be_falsey
       end
 
       it 'should not do anything if no params' do
-        @poll.waiting!
+        poll.waiting!
         post :move_up
         expect(response).to have_http_status(:bad_request)
-        expect(@poll.reload.in_progress?).to be_falsey
+        expect(poll.reload.in_progress?).to be_falsey
       end
     end
 
     describe 'POST delete' do
-      before (:each) do
-        @poll = create(:poll, status: :active)
-      end
-
       it 'should delete post if admin' do
-        @user.update(admin: true)
-        post :delete, params: { id: @poll.id }
+        user.update(admin: true)
+        post :delete, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
         expect(Poll.count).to eq(0)
       end
 
       it 'should not delete post if non admin' do
-        post :delete, params: { id: @poll.id }
+        post :delete, params: { id: poll.id }
         expect(response).to have_http_status(:ok)
         expect(Poll.count).to eq(1)
       end
