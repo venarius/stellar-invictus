3510420bb162b2cf4ab5493d50e298d7163edf64
Ben Sharpe <bsharpe@gmail.com>
2019-04-06 07:50:05 -0600

    convert missions_controller to exceptions

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,68 +1,53 @@
 class MissionsController < ApplicationController
 
-  before_action :get_mission, except: [:popup]
-
-  # Render Info of mission
   def info
-    if @mission.offered? || (@mission.active? && (@mission.user == current_user))
-      render(partial: 'stations/missions/info', locals: { mission: @mission }) && (return)
-    else
-      render json: {}, status: :bad_request
-    end
+    raise InvalidRequest if !mission.offered? && !(mission.active? && (mission.user_id == current_user.id))
+
+    render partial: 'stations/missions/info', locals: { mission: mission }, status: :ok
   end
 
-  # Accept a mission
   def accept
-    if @mission&.offered? && (@mission.location == current_user.location) && (current_user.missions.count < 5)
+    raise InvalidRequest unless mission.offered?
+    raise InvalidRequest unless mission.location_id == current_user.location_id
+    raise InvalidRequest unless current_user.missions.count < 5
 
-      Item::GiveToUser.(user: current_user, location: @mission.location, loader: @mission.mission_loader, amount: @mission.mission_amount) if @mission.delivery?
+    Item::GiveToUser.(user: current_user, location: mission.location, loader: mission.mission_loader, amount: mission.mission_amount) if mission.delivery?
 
-      @mission.active! && @mission.update(user_id: current_user.id)
+    mission.active! && mission.update(user_id: current_user.id)
 
-      MissionGenerator.generate_missions(current_user.location.id)
+    MissionGenerator.generate_missions(current_user.location.id)
 
-      render(json: { message: I18n.t('missions.successfully_accepted_mission') }, status: :ok) && (return)
-    end
-    render json: {}, status: :bad_request
+    render json: { message: I18n.t('missions.successfully_accepted_mission') }, status: :ok
   end
 
-  # Finish a mission
   def finish
-    error = MissionGenerator.finish_mission(@mission.id)
-    if error
-      render json: { 'error_message': error }, status: :bad_request
-    else
-      render json: { message: I18n.t('missions.successfully_finished_mission') }, status: :ok
-    end
+    error = MissionGenerator.finish_mission(mission.id)
+    raise InvalidRequest.new(error) if error
+
+    render json: { message: I18n.t('missions.successfully_finished_mission') }, status: :ok
   end
 
-  # Abort a mission
   def abort
-    if @mission.active? && (@mission.user == current_user)
-      error = MissionGenerator.abort_mission(@mission.id)
-      if error
-        render json: { 'error_message': error }, status: :bad_request
-      else
-        render json: { message: I18n.t('missions.successfully_aborted_mission') }, status: :ok
-      end
-    else
-      render json: {}, status: :bad_request
-    end
+    raise InvalidRequest if !mission.active? || (mission.user_id != current_user.id)
+
+    error = MissionGenerator.abort_mission(mission.id)
+    raise InvalidRequest.new(error) if error
+
+    render json: { message: I18n.t('missions.successfully_aborted_mission') }, status: :ok
   end
 
-  # Popup
   def popup
     render partial: '/stations/missions/popup'
   end
 
   private
 
-  def get_mission
-    if params[:id]
-      @mission = Mission.ensure(params[:id])
-      unless @mission && current_user.docked
-        render(json: {}, status: :bad_request) && (return)
-      end
+  def mission
+    raise InvalidRequest unless current_user.docked?
+    @mission ||= begin
+      record = Mission.ensure(params[:id])
+      raise InvalidRequest unless record
+      record
     end
   end
 
