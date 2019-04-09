47eb03e97916469fba6ae67c6db4d89425780737
Ben Sharpe <bsharpe@gmail.com>
2019-04-09 10:10:51 -0600

    typo

--- app/services/rand_service.rb
+++ app/services/rand_service.rb
@@ -1,12 +1,12 @@
 class RandService
   ALPHA_CHARSET = Array('A'..'Z') + Array('a'..'z')
-  UPCASE_ALHPA_CHARSET = Array('A'..'Z')
+  UPCASE_ALPHA_CHARSET = Array('A'..'Z')
 
   def self.upcase_alpha(size)
-    Array.new(size) { UPCASE_ALHPA_CHARSET.sample }.join
+    Array.new(size) { UPCASE_ALPHA_CHARSET.sample }.join
   end
 
   def self.alpha(size)
-    Array.new(size) { ALHPA_CHARSET.sample }.join
+    Array.new(size) { ALPHA_CHARSET.sample }.join
   end
 end
