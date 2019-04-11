1dcf035e70357935b6295f5e148621f42c5dfddd
Ben Sharpe <bsharpe@gmail.com>
2019-04-11 05:10:02 -0600

    notes about sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
 Sidekiq.configure_server do |config|
+  # NOTE: While this may be necessary, this will cause Sidekiq to pound your Redis server
+  # polling for jobs - which is why Mike Perham says that Sidekiq isn't suited for precision timing
   config.average_scheduled_poll_interval = 0.05
 end
