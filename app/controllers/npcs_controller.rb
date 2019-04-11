37cec15910651bca65ec5b73e1225ae2f05b2af6
Ben Sharpe <bsharpe@gmail.com>
2019-04-11 06:19:16 -0600

    rubocop cleanup

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -1,10 +1,10 @@
 class NpcsController < ApplicationController
   def target
     target = Npc.ensure(params[:id])
-    raise InvalidRequest.new( time: current_user.active_spaceship.get_target_time ) unless target
-    raise InvalidRequest.new( time: current_user.active_spaceship.get_target_time ) unless current_user.can_be_attacked?
-    raise InvalidRequest.new( time: current_user.active_spaceship.get_target_time ) unless target.location_id == current_user.location_id
-    raise InvalidRequest.new( time: current_user.active_spaceship.get_target_time ) unless current_user.npc_target_id != target.id
+    raise InvalidRequest.new(time: current_user.active_spaceship.get_target_time) unless target
+    raise InvalidRequest.new(time: current_user.active_spaceship.get_target_time) unless current_user.can_be_attacked?
+    raise InvalidRequest.new(time: current_user.active_spaceship.get_target_time) unless target.location_id == current_user.location_id
+    raise InvalidRequest.new(time: current_user.active_spaceship.get_target_time) unless current_user.npc_target_id != target.id
 
     TargetNpcWorker.perform_async(current_user.id, target.id)
     render json: { time: current_user.active_spaceship.get_target_time }, status: :ok
